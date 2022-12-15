26ee2bfb904cba7ea377d34ca59e02159d75877f
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-12-15 16:11:34 +0530

    1 file modified

--- index.js
+++ index.js
@@ -11,12 +11,27 @@ appDiv.innerHTML = `<h1>JS Object</h1>`;
 // sayHi('Hemant! Welcome....');
 
 //Object
-//let usersData = 'https://jsonplaceholder.typicode.com/users';
-//let todosData = 'https://jsonplaceholder.typicode.com/todos/5';
+let usersData = 'https://jsonplaceholder.typicode.com/users';
+let todosData = 'https://jsonplaceholder.typicode.com/todos/5';
 let albumData = 'https://jsonplaceholder.typicode.com/albums';
+//this is the promis example
 sayBye(albumData)
   .then((data) => {
+    console.log('----------------promis example------------------');
     console.log('promise resolved', data);
+  })
+  .catch((err) => {
+    console.log('promise Reject', err);
+  });
+//this is the promis chaining example
+sayBye(usersData)
+  .then((data) => {
+    console.log('----------------promis chaining------------------');
+    console.log('promise1 resolved', data);
+    return sayBye(todosData);
+  })
+  .then((data) => {
+    console.log('promise2 resolved', data);
   })
   .catch((err) => {
     console.log('promise Reject', err);
