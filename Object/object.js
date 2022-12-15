6a1488e78869cd8a96885aebb2ab9893ac608f56
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-12-15 15:35:24 +0530

    2 files modified

--- Object/object.js
+++ Object/object.js
@@ -1,34 +1,20 @@
 function sayHi(user) {
   console.log(`Hello, ${user}!`);
 }
-
-function sayBye() {
-  let usersData = 'https://jsonplaceholder.typicode.com/users';
-
-  getTodos = (users, callBack) => {
-    return new Promise((resolve, reject) => {
-      const request = new XMLHttpRequest();
-      request.addEventListener('readystagechange', () => {
-        if ((request.readyState === 4, request.status === 200)) {
-          let data = JSON.parse(request.responseText);
-          resolve(data);
-        } else if (request.response === 4) {
-          reject('error getting resource');
-        }
-      });
-      request.open('GET', users);
-      request.send();
+var retdata = [];
+function sayBye(users) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        let data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('error getting resource');
+      }
     });
-  };
-  //call back hell setution
-  getTodos(usersData, (err, data) => {
-    console.log(data);
-    getTodos(todosData, (err, data) => {
-      console.log(data);
-      getTodos(albumData, (err, data) => {
-        console.log(data);
-      });
-    });
+    request.open('GET', users);
+    request.send();
   });
 }
 
--- index.js
+++ index.js
@@ -8,8 +8,16 @@ import { sayHi, sayBye } from './Object/object.js';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Object</h1>`;
 
-sayHi('Hemant! Welcome....');
+// sayHi('Hemant! Welcome....');
 
 //Object
-
-console.log(sayBye);
+//let usersData = 'https://jsonplaceholder.typicode.com/users';
+//let todosData = 'https://jsonplaceholder.typicode.com/todos/5';
+let albumData = 'https://jsonplaceholder.typicode.com/albums';
+sayBye(albumData)
+  .then((data) => {
+    console.log('promise resolved', data);
+  })
+  .catch((err) => {
+    console.log('promise Reject', err);
+  });
